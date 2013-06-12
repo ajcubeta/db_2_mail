4afc7dfea1741f62b8a5739151ebf74c12a7df4b
ajcubeta <ajcubeta@gmail.com>
2013-06-12 17:33:34 +0800

    Setup db_2_mail app for test

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'RedCloth'
+gem 'devise'
+gem 'postmark-rails'
+gem 'time_diff'
+gem 'will_paginate'
+gem 'wirble'
+gem 'hirb'
+gem 'awesome_print'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Db2Mail::Application.config.secret_token = 'd8fbbd65de534baf63f14c3b5b2c8dca4a0b4d35ff72006084de9f433f81a6e8fcc8ff74000b1e3051fe934433f4daba6b28024623bb6109c8142cbe55f4bc39'
